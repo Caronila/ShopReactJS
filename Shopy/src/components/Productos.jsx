cc18bff93f9567097570b53666f7faed8588e67d
Caronila <carolinacardoso92@gmail.com>
2023-11-21 00:05:55 -0300

    Add button to products
    Success message + refresh page after "buy"

--- Shopy/src/components/Productos.jsx
+++ Shopy/src/components/Productos.jsx
@@ -1,66 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Productos.css';
 
 const Productos = () => {
+  const [productos, setProductos] = useState([
+    { id: 1, comprado: false },
+    { id: 2, comprado: false },
+    { id: 3, comprado: false },
+    { id: 4, comprado: false },
+  ]);
+
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const handleCompraClick = (id) => {
+    const productoIndex = productos.findIndex((producto) => producto.id === id);
+
+    setProductos((prevProductos) => {
+      const nuevosProductos = [...prevProductos];
+      nuevosProductos[productoIndex] = { ...nuevosProductos[productoIndex], comprado: true };
+      return nuevosProductos;
+    });
+
+    setShowSuccessMessage(true);
+
+    setTimeout(() => {
+     
+      window.location.reload();
+    }, 3000);
+  };
+
   return (
     <main className='productos'>
       <h1>Nuestros productos</h1>
-    <div className="card-deck">
-      <div className="card">
-        <img className="card-img-top" src="src/assets/producto1.jpg" alt="Producto 1" />
-        <div className="card-body">
-          <h5 className="card-title">Producto 1</h5>
-          <p className="card-text">
-            descripción de producto 1
-          </p>
-
-          <button type="button" className="btn btn-primary">
-            Comprar
-          </button>
-        </div>
+      <div className="card-deck">
+        {productos.map((producto) => (
+          <div className="card" key={producto.id}>
+            <img className="card-img-top" src={`src/assets/producto${producto.id}.jpg`} alt={`Producto ${producto.id}`} />
+            <div className="card-body">
+              <h5 className="card-title">{`Producto ${producto.id}`}</h5>
+              <p className="card-text">{`Descripción de producto ${producto.id}`}</p>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => handleCompraClick(producto.id)}
+                disabled={producto.comprado}
+              >
+                {producto.comprado ? '¡Compra realizada!' : 'Comprar'}
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
-
-      <div className="card">
-        <img className="card-img-top" src="src/assets/producto4.jpg" alt="Producto 2" />
-        <div className="card-body">
-          <h5 className="card-title">Producto 2</h5>
-          <p className="card-text">
-            descripción de producto 2
-          </p>
-
-          <button type="button" className="btn btn-primary">
-            Comprar
-          </button>
+      {showSuccessMessage && (
+        <div className='success-message'>
+          ¡Haz comprado con éxito! La página se recargará en unos segundos.
         </div>
-      </div>
-      <div className="card">
-        <img className="card-img-top" src="src/assets/producto3.jpg" alt="Producto 3" />
-        <div className="card-body">
-          <h5 className="card-title">Producto 3</h5>
-          <p className="card-text">
-          descripción de producto 3
-          </p>
-
-          <button type="button" className="btn btn-primary">
-            Comprar
-          </button>
-        </div>
-      </div>
-      <div className="card">
-        <img className="card-img-top" src="src/assets/producto5.jpg" alt="Producto 4" />
-        <div className="card-body">
-          <h5 className="card-title">Producto 4</h5>
-          <p className="card-text">
-          descripción de producto 4
-          </p>
-
-          <button type="button" className="btn btn-primary">
-            Comprar
-          </button>
-        </div>
-      </div>
-
-    </div>
+      )}
     </main>
   );
 }
