3665d2ab22ca436d79efecda47d88fe98c931611
Caronila <carolinacardoso92@gmail.com>
2023-11-22 22:29:24 -0300

    Add conocenos page

--- Shopy/src/components/Conocenos.jsx
+++ Shopy/src/components/Conocenos.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import './Conocenos.css';
 
 const Conocenos = () => {
     return (
-
         <main>
-        <div>
-             <p>Conocenos</p>
-        </div>
+            <div>
+                <h1 className='conocenos'>Conócenos</h1>
+                <p className='conocenosP'>Bienvenido a Shopy, el lugar donde la armonía y el bienestar se encuentran. Nos dedicamos con pasión a ofrecerte una experiencia única en el fascinante mundo de la aromaterapia.</p>
+
+                <h1 className='conocenos' >Nuestra Historia</h1>
+                <p className='conocenosP'>En Shopy nuestra historia comienza con una profunda admiración por los regalos que la naturaleza nos brinda. Nos inspiramos en la esencia misma de las plantas, flores y frutas para crear productos que no solo embellecen tu entorno, sino que también nutren tu bienestar físico y emocional.
+                </p>
+                <h1 className='conocenos' >Nuestro Compromiso</h1>
+                <p className='conocenosP'>Nos enorgullece nuestra dedicación a la calidad y la autenticidad. Cada uno de nuestros productos es cuidadosamente elaborado utilizando ingredientes naturales y procesos sostenibles. Nos esforzamos por preservar la pureza de los aceites esenciales y la frescura de cada fragancia, asegurando que experimentes lo mejor de la naturaleza en cada producto que elijas.
+                </p>
+                <h1 className='conocenos' >Nuestra Misión</h1>
+                <p className='conocenosP'>En Shopy, nuestra misión va más allá de proporcionar productos excepcionales. Buscamos inspirar un cambio positivo en tu vida diaria. Creemos en la importancia de crear espacios que reflejen paz y equilibrio, transformando tu hogar en un santuario personal donde puedas recargar energías y encontrar serenidad.</p>
+
+                <h1 className='conocenos'>Descubre Nuestro Mundo</h1>
+                <p className='conocenosP'>Explora nuestra gama de productos, desde aceites esenciales hasta difusores exquisitamente diseñados. Cada artículo ha sido seleccionado con amor y cuidado para ofrecerte una experiencia sensorial única. Sumérgete en nuestra historia, descubre nuestros valores y déjanos acompañarte en este viaje hacia el bienestar.</p>
+
+                <p className='conocenosB'>En Shopy, te damos la bienvenida a nuestra comunidad. ¡Descubre la magia de la aromaterapia y permite que Shopy sea tu compañero en la búsqueda de un estilo de vida más armonioso!</p>
+            </div>
         </main>
-        
     );
 }
 
